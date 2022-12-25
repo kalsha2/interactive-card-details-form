a75d958c39c16184bd168584a2312dbfe7813d2e
Kaleab <babywondu@gmail.com>
2022-12-25 20:47:46 +0300

    Added the form and styled it for the desktop version

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('Hello World');
+
+function Submitted() {
+    var form = document.getElementsByClassName("theform");
+    var cardHolder = document.getElementById("holder");
+    cardHolder.style.display = "none";
+}
