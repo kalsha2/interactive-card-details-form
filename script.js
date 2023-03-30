45277e99650e5c9be1895893034336dada000777
Kaleab <babywondu@gmail.com>
2023-03-30 21:27:23 +0300

    fixed the button and i made it to be responsive for mobile phones

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 console.log('Hello World');
+var cont = document.querySelector(".btn");
+cont.onclick = Submitted;
 
 function Submitted() {
-    var form = document.getElementsByClassName("theform");
-    var cardHolder = document.getElementById("holder");
-    cardHolder.style.display = "none";
+    var myform = document.querySelector(".theform");
+    var thanks= document.querySelector(".Thanks")
+    thanks.style.display = "none";
+    myform.style.display = "block";
+
 }
